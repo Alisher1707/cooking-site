b8e6ccb2391a62c9cb2b15cae5368ffb458620ed
Alisher1707 <abdusalomova941@gmail.com>
2025-11-05 14:16:35 +0500

    Add image of "pechda pishirilgan gosht" to the public img directory

--- src/components/FanFavorites.jsx
+++ src/components/FanFavorites.jsx
@@ -7,18 +7,18 @@ const FanFavorites = () => {
   const t = translations[language];
 
   const favorites = [
-    { id: 1, image: '/img/kabob-1.jpg', title: "PECHDA PISHIRILGAN QOVURG'ALAR" },
-    { id: 2, image: '/img/category-1.webp', title: 'MOZZARELLA TAYOQCHALARI' },
-    { id: 3, image: '/img/page-1.webp', title: '5 DAQIQALIK VEGAN PANCAKE' },
-    { id: 4, image: '/img/page-2.webp', title: 'SHAKSHUKA' },
-    { id: 5, image: '/img/kabob-2.jpg', title: 'ODDIY PECHDA PISHIRILGAN DENGIZ BASS' },
-    { id: 6, image: '/img/chicken.jpg', title: "GO'SHT VA BROCCOLI QOVURMA" },
-    { id: 7, image: '/img/category-2.webp', title: 'FAJITAS' },
-    { id: 8, image: '/img/category-3.webp', title: 'QAYMOGʻLI KREM' },
-    { id: 9, image: '/img/kabob-3.jpg', title: 'SABZAVOTLI QOVURMA' },
-    { id: 10, image: '/img/osh.webp', title: "O'ZBEK OSHI" },
-    { id: 11, image: '/img/salat.webp', title: 'TOVUQLI SALAT' },
-    { id: 12, image: '/img/page-4.webp', title: 'MEVALI DESERT' },
+    { id: 1, image: '/img/pechda pishirilgan gosht.jpg', titleKey: 'fanRecipe1' },
+    { id: 2, image: '/img/MOZZARELLA TAYOQCHALARI.jpg', titleKey: 'fanRecipe2' },
+    { id: 3, image: '/img/page-1.webp', titleKey: 'fanRecipe3' },
+    { id: 4, image: '/img/page-2.webp', titleKey: 'fanRecipe4' },
+    { id: 5, image: '/img/kabob-2.jpg', titleKey: 'fanRecipe5' },
+    { id: 6, image: "/img/GO'SHT VA BROCCOLI QOVURMA.jpg", titleKey: 'fanRecipe6' },
+    { id: 7, image: '/img/FAJITAS.jpg', titleKey: 'fanRecipe7' },
+    { id: 8, image: '/img/category-3.webp', titleKey: 'fanRecipe8' },
+    { id: 9, image: '/img/SABZAVOTLI QOVURMA.jpg', titleKey: 'fanRecipe9' },
+    { id: 10, image: '/img/osh.webp', titleKey: 'fanRecipe10' },
+    { id: 11, image: '/img/salat.webp', titleKey: 'fanRecipe11' },
+    { id: 12, image: '/img/MEVALI DESERT.jpg', titleKey: 'fanRecipe12' },
   ];
 
   return (
@@ -33,9 +33,9 @@ const FanFavorites = () => {
           {favorites.map((recipe) => (
             <div key={recipe.id} className="fan-card">
               <div className="fan-card-image-wrapper">
-                <img src={recipe.image} alt={recipe.title} className="fan-card-image" />
+                <img src={recipe.image} alt={t[recipe.titleKey]} className="fan-card-image" />
               </div>
-              <h3 className="fan-card-title">{recipe.title}</h3>
+              <h3 className="fan-card-title">{t[recipe.titleKey]}</h3>
             </div>
           ))}
         </div>
